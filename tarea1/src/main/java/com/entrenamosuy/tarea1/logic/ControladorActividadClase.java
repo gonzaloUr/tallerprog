1e28fbfb291191597d6f4d3092053bd26525b494
sapog0rdo <emiliovallelisboa@gmail.com>
2021-08-23 16:34:29 -0300

    Intento de avance

--- tarea1/src/main/java/com/entrenamosuy/tarea1/logic/ControladorActividadClase.java
+++ tarea1/src/main/java/com/entrenamosuy/tarea1/logic/ControladorActividadClase.java
@@ -2,4 +2,31 @@ package com.entrenamosuy.tarea1.logic;
 
 public class ControladorActividadClase {
     
+    public:
+    ingresarDatosActividad(String nombreInstitutucion,String nombre, String descripcion, float duracion, float costo, Fecha fecha){
+        Manejador maneja = Manejador.getInstance();
+        Map actividades = maneja.getActividades();
+        try {
+            if actividades.containsKey(nombre){
+                throw();
+            }
+            else {
+                
+                actividades.put(nombre,nuevaActividad); //agrego la nueva actividad al map
+            }
+        } 
+        catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
+
