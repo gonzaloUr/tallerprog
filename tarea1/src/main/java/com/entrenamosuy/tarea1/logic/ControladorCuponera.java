ee726070d06cb56093e2d917ce8affdc580ca83d
Gonzalo Hugo Uran Demarco <gonzalo.uran@fing.edu.uy>
2021-08-23 17:33:42 -0300

    Add fabrica

--- tarea1/src/main/java/com/entrenamosuy/tarea1/logic/ControladorCuponera.java
+++ tarea1/src/main/java/com/entrenamosuy/tarea1/logic/ControladorCuponera.java
@@ -1,5 +1,47 @@
 package com.entrenamosuy.tarea1.logic;
 
-public class ControladorCuponera {
-    
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.entrenamosuy.tarea1.data.DataCuponera;
+import com.entrenamosuy.tarea1.util.Pair;
+
+public class ControladorCuponera implements IControladorCuponera {
+
+    @Override
+    public void crearCuponera(String nombre, String descripcion, LocalDateTime inicio, LocalDateTime fin,
+            int descuento) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Set<Pair<String, String>> cuponerasUsables(String clase, String socio) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Pair<String, String>> actividadesAgregables(String cuponera, String institucion) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void agregarACuponera(String cuponera, String actividad) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Set<Pair<String, String>> obtenerDescCuponeras() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DataCuponera consultarCuponera(String nombre) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
