4147900aabc84732464a6610cdcb752facbf4640
Luciano <luciano.almenares@gmail.com>
2021-08-24 18:44:37 -0300

    Operaciones controlador Usuario

--- tarea1/src/main/java/com/entrenamosuy/tarea1/logic/ControladorUsuario.java
+++ tarea1/src/main/java/com/entrenamosuy/tarea1/logic/ControladorUsuario.java
@@ -71,26 +71,64 @@ public class ControladorUsuario implements IControladorUsuario {
     }
 
     @Override
-    public Set<Triple<String, String, String>> obtenerDescSocios() {
-        // TODO Auto-generated method stub
-        return null;
+    public Set<Triple<String, String, String>> obtenerDescSocios() throws SociosVacioException {
+        Manejador man = Manejador.getInstance();
+        Map<String, Socio> mapa = man.getSocios();
+        if (mapa != null) {
+            Set<Triple<String, String, String>> res;
+            Iterator<Entry<String, Socio>> it = mapa.entrySet().iterator();
+            while(it.hasNext()) {
+                Socio s = (Socio) it.next();
+                Triple<String, String, String> trip = new Triple<String, String, String>(s.getNombre(), s.getNickname(), s.getApellido());
+                res.add(trip);
+            }
+            return res;
+        }
+        else  
+            throw new SociosVacioException("No hay socios en el sistema");
     }
 
     @Override
-    public Set<Triple<String, String, String>> obtenerDescProfesores() {
-        // TODO Auto-generated method stub
-        return null;
+    public Set<Triple<String, String, String>> obtenerDescProfesores() throws ProfesoresVacioException {
+        Manejador man = Manejador.getInstance();
+        Map<String, Profesor> mapa = man.getProfesores();
+        if (mapa != null) {
+            Set<Triple<String, String, String>> res;
+            Iterator<Entry<String, Profesor>> it = mapa.entrySet().iterator();
+            while(it.hasNext()) {
+                Profesor p = (Profesor) it.next();
+                Triple<String, String, String> trip = new Triple<String, String, String>(p.getNombre(), p.getNickname(), p.getApellido());
+                res.add(trip);
+            }
+            return res;
+        }
+        else  
+            throw new ProfesoresVacioException("No hay profesores en el sistema");
     }
 
     @Override
-    public DataUsuario consultarSocio(String nombre) {
-        // TODO Auto-generated method stub
-        return null;
+    public DataUsuario consultarSocio(String nickname) throws SocioNoExisteException{
+        Manejador man = Manejador.getInstance();
+        Map<String, Socio> mapa = man.getSocio();
+        Socio p = mapa.get(nickname);
+        if (p != null) {
+            DataUsuario r = p.getDataUsuario(); //Implementarlo en la clase Usuario/Socio
+            return r;
+        }
+        else
+            throw new SocioNoExisteException("No existe un socio con ese nickname en el sistema");
     }
 
     @Override
-    public DataProfesor consultarProfesor(String nombre) {
-        // TODO Auto-generated method stub
-        return null;
+    public DataProfesor consultarProfesor(String nickname) throws ProfesorNoExisteException{
+        Manejador man = Manejador.getInstance();
+        Map<String, Profesor> mapa = man.getProfesores();
+        Profesor p = mapa.get(nickname);
+        if (p != null) {
+            DataProfesor r = p.getDataProfesor(); //Implementarlo en la clase Profesor
+            return r;
+        }
+        else
+            throw new ProfesorNoExisteException("No existe un profesor con ese nickname en el sistema");
     }
 }
