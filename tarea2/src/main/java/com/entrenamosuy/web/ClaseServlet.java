61224dd1eea40bac97de5b855c4194bda7b52517
gonzalo.uran <gonzalo.uran@fing.edu.uy>
2021-10-24 21:45:35 -0300

    alta dictado clase

--- tarea2/src/main/java/com/entrenamosuy/web/ClaseServlet.java
+++ tarea2/src/main/java/com/entrenamosuy/web/ClaseServlet.java
@@ -1,37 +1,41 @@
 package com.entrenamosuy.web;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
-import com.entrenamosuy.core.AbstractFacadeClase;
-import com.entrenamosuy.core.AbstractFacadeCuponera;
-import com.entrenamosuy.core.AbstractFacadeActividad;
-import com.entrenamosuy.core.exceptions.ClaseInconsistenteException;
-import com.entrenamosuy.core.exceptions.SocioNoEncontradoException;
-import com.entrenamosuy.core.exceptions.RegistroInconsistenteException;
-import com.entrenamosuy.core.data.DataInstitucion;
-import com.entrenamosuy.core.data.DataUsuario;
-import com.entrenamosuy.core.data.DataSocio;
-import com.entrenamosuy.core.data.DescProfesor;
-import com.entrenamosuy.core.data.DataActividad;
 import com.entrenamosuy.core.data.DataClase;
+import com.entrenamosuy.core.data.DataProfesor;
+import com.entrenamosuy.core.data.DataSocio;
+import com.entrenamosuy.core.data.DataUsuario;
+import com.entrenamosuy.core.data.DescProfesor;
+import com.entrenamosuy.core.exceptions.ClaseInconsistenteException;
+import com.entrenamosuy.core.exceptions.RegistroInconsistenteException;
+import com.entrenamosuy.core.exceptions.SocioNoEncontradoException;
 
-
-
+@MultipartConfig(fileSizeThreshold=1024*1024*10, maxFileSize=1024*1024*50, maxRequestSize=1024*1024*100)
 public class ClaseServlet extends HttpServlet {
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getServletPath();
-        
+
 		if(path.equals("/registrarse_a_clase_1")) {
             Set<String> inst = Facades
 				.getFacades()
@@ -41,13 +45,13 @@ public class ClaseServlet extends HttpServlet {
 			Set<String> cats = Facades
 				.getFacades()
 				.getFacadeActividad()
-				.getCategorias();	
+				.getCategorias();
 
 			request.setAttribute("instituciones", inst);
 			request.setAttribute("categorias", cats);
 
 			request.getRequestDispatcher("/registrarse_a_clase_1.jsp")
-				.forward(request, response);	
+				.forward(request, response);
 
 		} else if (path.equals("/registrarse_a_clase_2")){
 			String inst = request.getParameter("institucion");
@@ -70,8 +74,8 @@ public class ClaseServlet extends HttpServlet {
 
 			request.getRequestDispatcher("/registrarse_a_clase_2.jsp")
 				.forward(request, response);
-		
-		} else if (path.equals("/registrarse_a_clase_3")){ 
+
+		} else if (path.equals("/registrarse_a_clase_3")){
 			String acti = request.getParameter("actividad");
 
 			Set<String> clases = Facades
@@ -86,10 +90,10 @@ public class ClaseServlet extends HttpServlet {
 
 
 		} else if(path.equals("/confirmar_registro_clase")) {
-			
+
             String act = request.getParameter("actividad");
             //String cla = request.getParameter("clase");
-			
+
             HttpSession session = request.getSession();
 
             DataUsuario usr = (DataUsuario) session.getAttribute("usuario");
@@ -107,10 +111,10 @@ public class ClaseServlet extends HttpServlet {
 		} else if(path.equals("/consulta_dictado_clase")){
 
 			String claseNombre = request.getParameter("clase");
-			
+
 			HttpSession session = request.getSession();
             DataUsuario usr = (DataUsuario) session.getAttribute("usuario");
-			if (usr == null) 
+			if (usr == null)
 				request.setAttribute("es_socio", false);
 			else {
 				String nickname = usr.getNickname();
@@ -125,9 +129,9 @@ public class ClaseServlet extends HttpServlet {
 				}
 				if (esSocio)
 					request.setAttribute("es_socio", true);
-				else 
+				else
 					request.setAttribute("es_socio", false);
-			}		
+			}
 
             DataClase clase = Facades
                 .getFacades()
@@ -148,8 +152,8 @@ public class ClaseServlet extends HttpServlet {
                 .map(DescProfesor::getApellido)
                 .collect(Collectors.toSet());
 
-            String apellido = profesorApe.iterator().next();    
-                
+            String apellido = profesorApe.iterator().next();
+
             request.setAttribute("nombre", nombre);
             request.setAttribute("inicio", inicio);
             request.setAttribute("cantMin", cantMin);
@@ -159,24 +163,44 @@ public class ClaseServlet extends HttpServlet {
             request.setAttribute("profesorNom", profesorNom);
             request.setAttribute("profesorApe", profesorApe);
             request.setAttribute("apellido", apellido);
-			
+
 			request.getRequestDispatcher("/consulta_dictado_clase.jsp")
 			.forward(request, response);
 
-		} else {
+		} else if (path.equals("/alta_dictado_clase")) {
+            Boolean esProfesor = (Boolean) request.getSession().getAttribute("es_profesor");
 
-		}
+            if (esProfesor == null || !esProfesor)
+                throw new IllegalArgumentException("profesor no logeado");
+
+            DataProfesor profesor = (DataProfesor) request.getSession().getAttribute("usuario");
+            String institucion = profesor.getInstitucion();
+            Set<String> profesSet = Facades.getFacades().getFacadeUsuario().getProfesoresDeInstitucion(institucion);
+            Set<String> actividadesSet = Facades.getFacades().getFacadeActividad().getActividadesDeInstitucion(institucion);
+
+            List<String> profes = new ArrayList<>(profesSet.size());
+            List<String> actividades = new ArrayList<>(actividadesSet.size());
+
+            profes.addAll(profesSet);
+            actividades.addAll(actividadesSet);
+
+            request.setAttribute("institucion", institucion);
+            request.setAttribute("actividades", actividades);
+            request.setAttribute("profes", profes);
+
+			request.getRequestDispatcher("/alta_dictado_clase.jsp").forward(request, response);
+        }
     }
-    
 
-	
+
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getServletPath();
         if(path.equals("/confirmar_registro_clase")) {
-           
+
 			HttpSession session = request.getSession();
-        	
+
 			DataUsuario usr = (DataUsuario) session.getAttribute("usuario");
 			String nickname = usr.getNickname();
 
@@ -184,16 +208,16 @@ public class ClaseServlet extends HttpServlet {
 		    String cup = request.getParameter("cuponera");
 			LocalDate fecha = LocalDate.now();
 			response.getWriter().println("antes if");
-			if (cup == null) { 
+			if (cup == null) {
 				response.getWriter().println("cup null");
 				try {
 					Facades
 					.getFacades()
 					.getFacadeActividad()
 					.registarseSinCuponera(nickname, cla, fecha);
-					
 
-				} catch (RegistroInconsistenteException e) { 
+
+				} catch (RegistroInconsistenteException e) {
 					e.printStackTrace(response.getWriter());
 					request.setAttribute("reg_exito", false);
 					request.getRequestDispatcher("confirmar_registro_clase.jsp").forward(request, response);
@@ -220,9 +244,57 @@ public class ClaseServlet extends HttpServlet {
 			}
 			response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/"));
 
-        } 
+        } else if (path.equals("/alta_dictado_clase")) {
+            String actividad = request.getParameter("actividad");
+            String nombre = request.getParameter("nombre");
+            LocalDateTime inicio = LocalDateTime.parse(request.getParameter("inicio"));
+            int cantMin = Integer.parseInt(request.getParameter("cant_min"));
+            int cantMax = Integer.parseInt(request.getParameter("cant_max"));
+            URL acceso = new URL(request.getParameter("acceso"));
+            LocalDate registro = LocalDate.parse(request.getParameter("fecha_registro"));
+            Set<String> profes = Arrays.asList(request.getParameterValues("profes"))
+                .stream()
+                .collect(Collectors.toSet());
+
+            Part imgPart = request.getPart("img");
+            InputStream is = imgPart.getInputStream();
+
+            File tmp = File.createTempFile("img_", null);
+            OutputStream os = new FileOutputStream(tmp);
+
+            pipe(is, os);
+            os.close();
+
+            try {
+                Facades.getFacades().getFacadeClase().crearClase()
+                    .setNombreActividad(actividad)
+                    .setNombre(nombre)
+                    .setInicio(inicio)
+                    .setNicknameProfesores(profes)
+                    .setCantMin(cantMin)
+                    .setCantMax(cantMax)
+                    .setAcceso(acceso)
+                    .setFechaRegistro(registro)
+                    .setImagen(tmp)
+                    .invoke();
+            } catch (ClaseInconsistenteException e) {
+                // TODO: terminar.
+                e.printStackTrace(response.getWriter());
+                return;
+            }
+
+			response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/"));
+        }
     }
-	
+
+    private static void pipe(InputStream is, OutputStream os) throws IOException {
+        int n;
+        byte[] buff = new byte[1024];
+
+        while ((n = is.read(buff)) > -1)
+            os.write(buff, 0, n);
+    }
+
 }
 
 
--- tarea2/src/main/webapp/alta_dictado_clase.jsp
+++ tarea2/src/main/webapp/alta_dictado_clase.jsp
@@ -1,28 +1,50 @@
+<%@page import="java.util.Set"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 	<head>
         <meta charset="UTF-8">
         <title>Entrenamos.uy</title>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="styles/alta_usuario.css">
     </head>
-	<body class="d-flex flex-column text-center justify-content-center py-12">
-        <form class="d-flex flex-column form-signup" action="index.html">
-            <h1 class="h3 mb-3 fw-normal">Alta de dictado de clase</h1>
-            <label for="categorias">Elija una actividad deportiva de Telon:</label>
-            <select name="actividades_depor" id="actividades_depor" multiple>
-                <option value="Voleibol">Voleibol</option>
-                <option value="Atletismo">Atletismo</option>
-                <option value="Basquetbol">Basquetbol</option>
-            </select> 
-            <p> </p>
-            <input class="form-control mb-3" placeholder="Nombre" name="nombre">
-            <input class="form-control mb-3" placeholder="Fecha inicio" name="Fecha_inicio">
-            <input class="form-control mb-3" placeholder="Hora inicio" name="Hora_inicio">
-            <input class="form-control mb-3" placeholder="Socios mínimos" name="Socios_minimos">
-            <input class="form-control mb-3" placeholder="Socios máximos" name="Socios_maximos">
-            <input class="form-control mb-3" placeholder="URL de pagina web" name="URL">
-            <input class="w-100 btn btn-primary" type="submit" value="Dar de alta">
-        </form>
+	<body class="container">
+        <div class="col-12 p-5">
+            <form class="d-flex flex-column" method="post" action="alta_dictado_clase" enctype="multipart/form-data">
+                <h1 class="h3 mb-3 fw-normal">Alta de clase</h1>
+                <select class="form-control mb-3" name="actividad">
+                    <c:forEach var="i" begin="0" end="${actividades.size() - 1}">
+                        <c:choose>
+                            <c:when test="${i eq 0}">
+                                <option selected value="${actividades.get(i)}">${actividades.get(i)}</option>
+                            </c:when>
+                            <c:otherwise>
+                                <option value="${actividades.get(i)}">${actividades.get(i)}</option>
+                            </c:otherwise>
+                        </c:choose>
+                    </c:forEach>
+                </select>
+                <input class="form-control mb-3" placeholder="nombre" name="nombre">
+                <input class="form-control mb-3" placeholder="inicio" name="inicio" type="datetime-local">
+                <select class="form-control mb-3" name="profes" multiple>
+                    <c:forEach var="i" begin="0" end="${profes.size() - 1}">
+                        <c:choose>
+                            <c:when test="${i eq 0}">
+                                <option selected value="${profes.get(i)}">${profes.get(i)}</option>
+                            </c:when>
+                            <c:otherwise>
+                                <option value="${profes.get(i)}">${profes.get(i)}</option>
+                            </c:otherwise>
+                        </c:choose>
+                    </c:forEach>
+                </select>
+                <input class="form-control mb-3" placeholder="cantidad mininima" name="cant_min" type="number" min="0">
+                <input class="form-control mb-3" placeholder="cantidad maxima" name="cant_max" type="number" min="0">
+                <input class="form-control mb-3" placeholder="url de acesso" name="acceso" type="url">
+                <input class="form-control mb-3" placeholder="fecha de registro" name="fecha_registro" type="date">
+                <input class="form-control mb-3" accept="image/*" name="img" type="file">
+                <input class="btn btn-primary w-100" type="submit" value="Crear">
+            </form>
+        </div>
 	</body>
 </html>
